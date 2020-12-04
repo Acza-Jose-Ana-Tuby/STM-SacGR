c98bbe5cb36317bd2f477d3553adf92143813f2b
José de Senna <jspneto@ufpa.br>
2020-12-03 21:32:53 -0300

    Adiciona script de conexão Flask-MySQL

---
+++ teste-projeto-labes/main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'sistema-agendamento'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    cursor = mysql.connection.cursor()
+    #cursor.execute("INSERT INTO Usuarios(nome, RG, CPF, endereço, grupo_risco, ID) VALUES ('Tuby', '7421806', '70194401260', 'Boaventura', 0, 1)")
+    #ursor.execute("INSERT INTO Usuarios(nome) VALUES ('Tuby')")
+    cursor.execute("SELECT * FROM Usuarios")
+    result = cursor.fetchall()
+    mysql.connection.commit()
+    cursor.close()
+    return str(result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
